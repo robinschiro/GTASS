df517dfc37016a61a27b551cd7874013e61e0f56
Raphael Saint-Louis <raphaelstlouis@gmail.com>
2016-03-29 15:19:35 -0400

    added a new div element so the username/password/login_box don't touch the left edge of the outer rectanglular div element

--- www/view/index.php
+++ www/view/index.php
@@ -1,7 +1,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link href="index.css" type="text/css" rel="stylesheets">
+    <link href="public/stylesheets/index.css" type="text/css" rel="stylesheet">
     <title>GTASS Login</title>
 </head>
 
@@ -15,14 +15,16 @@
         User Login
     </p>
     <form action="/login" method="POST">
-        <!--    <p class="loginIn">-->
-        username: <input id="username" type="text" name="username" class="textBoxes" required="required">
-        <br><br>
-        password: <input id="password" type="password" name="password" class="textBoxes2" required="required">
-        <br><br>
+        <div class="inputBoxBorders">
+            username: <input id="username" type="text" name="username" class="textBoxes" required="required">
+            <br><br>
+            password: <input id="password" type="password" name="password" class="textBoxes2" required="required">
+            <br><br>
+            <input type="submit" value="Login">
+        </div>
         <input type="hidden" name="login">
         <!--    <p class="submit" align="center">-->
-        <input type="submit" value="Login">
+
 
         <!--    </p>-->
     </form>
