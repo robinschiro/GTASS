9c2801b75b9c47e1a4341cf1809fe5df4a22bbdc
Raphael Saint-Louis <raphaelstlouis@gmail.com>
2016-04-11 14:49:16 -0400

    add nominees STILL doesn't work and I have no idea why, but this fixes other issues with addNominees

--- www/view/addNomineesForm.php
+++ www/view/addNomineesForm.php
@@ -50,7 +50,7 @@ if($_SESSION['role'] != 3)
         in email use pid to create url
             - /nomineeApplication?id={PID}
      -->
-     <p class="addNomineeForm" align="left">
+     <p class="Form" align="left">
          Add Nominees
 
         <form action="/nomCtrl" method="post">
@@ -63,15 +63,15 @@ if($_SESSION['role'] != 3)
         </p>
 
         <div id="dynamicInput">
-            <label for="fname">First Name: </label>
+            First Name:
             <input type="text" name="fname[0]" placeholder="Nominee's First Name" id="fname">
-            <label for="lname">Last Name: </label>
+            Last Name:
             <input type="text" name="lname[0]" placeholder="Nominee's Last Name" id="lname">
-            <label for="pid">PID: </label>
+            PID:
             <input type="text" name="pid[0]" placeholder="Nominee's PID" id="pid">
-            <label for="email">Email: </label>
+            Email:
             <input type="text" name="email[0]" placeholder="Nominee's Email" id="email"><br>
-            <label for="rank">Rank Nominee: </label>
+            Nominee's Rank:
             <input type="number" name="rank[0]" placeholder="Rank" min="0" max="100" id="rank">
 
             CS Graduate:
@@ -93,10 +93,10 @@ if($_SESSION['role'] != 3)
         <div id="extraInputs">
         </div>
 
-        <br>
-
         <!-- button for adding values dynamically -->
-        <input type="button" value="Add Another Nominee" onClick="addInputAddNominee('extraInputs');">
+        <input type="button" value="Add Another Nominee" onClick="addInput('extraInputs');"><br><br>
+
+        <br><br>
 
         <!-- Tells the controller which function to call -->
         <input type="hidden" name="createNominators">
