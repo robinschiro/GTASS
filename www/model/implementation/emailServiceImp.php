3c6bc983cf4da3561a1eac30eab55b91981bdd56
Robin Schiro <robinschiro@gmail.com>
2016-04-13 16:49:43 -0400

    Switch email implementation to SendGrid to eliminate quota.
    Signed-off-by: Robin Schiro <robinschiro@gmail.com>

--- www/model/implementation/emailServiceImp.php
+++ www/model/implementation/emailServiceImp.php
@@ -10,6 +10,7 @@
 use \google\appengine\api\mail\Message;
 
 require_once('model/emailService.php');
+require 'resources/sendgrid-google-php/SendGrid_loader.php';
 
 class emailServiceImp implements emailService
 {
@@ -45,10 +46,10 @@ class emailServiceImp implements emailService
 
     function sendEmailtoNominatorsandGC($to, $data)
     {
-
         //data will store necessary data for email service
         $username = $data[0];
         $password = $data[1];
+        $subject = "Welcome to GTASS!";
 
         $body = "You are now a Nominator or GC member!<br><br>
                  Username:  $username <br> 
@@ -56,50 +57,84 @@ class emailServiceImp implements emailService
                  Follow the link below to change your account credentials:<br><br>
                  gtass-1256.appspot.com?goToAccount";
 
-        try {
-            $message = new Message();
-            $message->setSender("noreply@gtass-1256.appspotmail.com");
-            $message->addTo($to);
-            $message->setSubject("Welcome to GTASS!");
-            $message->setHtmlBody($body);
-            $message->send();
-        } catch (InvalidArgumentException $e) {
-            echo $e->getMessage() . 'Recipient: ' . $to;
-        }
+        $this->sendEmailThroughSendGrid($to, $subject, $body);
+
+//        try {
+//            $message = new Message();
+//            $message->setSender("noreply@gtass-1256.appspotmail.com");
+//            $message->addTo($to);
+//            $message->setSubject($subject);
+//            $message->setHtmlBody($body);
+//            $message->send();
+//        } catch (InvalidArgumentException $e) {
+//            echo $e->getMessage() . 'Recipient: ' . $to;
+//        }
     }
 
     function sendEmailtoNominees($to, $data)
     {
         $body = "You have been nominated to become a GTA!<br>Follow the link below:<br><br>gtass-1256.appspot.com/nomineeForm?pid=" . $data;
+        $subject = "You have been nominated to become a GTA!";
 
-        try {
-            $message = new Message();
-            $message->setSender("noreply@gtass-1256.appspotmail.com");
-            $message->addTo($to);
-            $message->setSubject("You have been nominated to become a GTA!");
-            $message->setHtmlBody($body);
-            $message->send();
-        } catch (InvalidArgumentException $e) {
-            echo $e->getMessage();
-        }
+        $this->sendEmailThroughSendGrid($to, $subject, $body);
+
+//        try {
+//            $message = new Message();
+//            $message->setSender("noreply@gtass-1256.appspotmail.com");
+//            $message->addTo($to);
+//            $message->setSubject($subject);
+//            $message->setHtmlBody($body);
+//            $message->send();
+//        } catch (InvalidArgumentException $e) {
+//            echo $e->getMessage();
+//        }
     }
 
     function nominee2dayDeadlineReminder($to, $data)
     {
+        $subject = "Deadline to respond to your GTA nomination is 2 days away!";
         $body = "Hello " . $data[0] . " " . $data[1] . ",<br><br>
                 The deadline to respond to your GTA nomination is in 2 days! Please<br>
                 go to the following link to fill out your application.<br><br>
                 gtass-1256.appspot.com/nomineeForm?pid=" . $data[2];
 
-        try {
-            $message = new Message();
-            $message->setSender("noreply@gtass-1256.appspotmail.com");
-            $message->addTo($to);
-            $message->setSubject("Deadline to respond to your GTA nomination is 2 days away!");
-            $message->setHtmlBody($body);
-            $message->send();
-        } catch (InvalidArgumentException $e) {
-            echo $e->getMessage();
+        $this->sendEmailThroughSendGrid($to, $subject, $body);
+
+//        try {
+//            $message = new Message();
+//            $message->setSender("noreply@gtass-1256.appspotmail.com");
+//            $message->addTo($to);
+//            $message->setSubject($subject);
+//            $message->setHtmlBody($body);
+//            $message->send();
+//        } catch (InvalidArgumentException $e) {
+//            echo $e->getMessage();
+//        }
+    }
+
+    function sendEmailThroughSendGrid($to, $subject, $body)
+    {
+        try
+        {
+            // Connect to your SendGrid account
+            $sendgrid = new SendGrid\SendGrid('gtasssendgrid', 'dbteam15');
+
+            // Make a message object
+            $mail = new SendGrid\Mail();
+
+            // Add recipients and other message details
+            $mail->addTo($to)->
+            setFrom('noreply@gtass-1256.appspotmail.com')->
+            setSubject($subject)->
+            setHtml($body);
+
+            // Use the Web API to send your message
+            $sendgrid->send($mail);
+        }
+        catch (Exception $ex)
+        {
+            echo 'Exception occurred when sending email through SendGrid:<br>';
+            print_r($ex);
         }
     }
 
